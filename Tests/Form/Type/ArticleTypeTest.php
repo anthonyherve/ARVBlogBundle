f05f7b4cb93eff7422e86ccb2fa5d2353f17ba40
Anthony HERVE <anthony.herve28@gmail.com>
2015-05-05 09:28:22 +0200

    Add unit tests for FormType

--- Tests/Form/Type/ArticleTypeTest.php
+++ Tests/Form/Type/ArticleTypeTest.php
@@ -3,8 +3,65 @@
 namespace ARV\BlogBundle\Tests\Form\Type;
 
 
+use ARV\BlogBundle\Entity\Article;
+use ARV\BlogBundle\Entity\Tag;
+use ARV\BlogBundle\Form\Type\ArticleType;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class ArticleTypeTest extends TypeTestCase {
+
+    /**
+     * @dataProvider getValidTestData
+     * @param $data
+     */
+    public function testSubmitData($data) {
+        $type = new ArticleType();
+        $form = $this->factory->create($type);
+
+        $object = new Article();
+        $object->setTitle($data['title']);
+        $object->setContent($data['content']);
+        foreach($data['tags'] as $tag) {
+            $object->addTag(new Tag($tag['name']));
+        }
+
+        $form->submit($data);
+
+        $this->assertTrue($form->isSynchronized());
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($data) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+
+    }
+
+    public function getValidTestData()
+    {
+        return array(
+            array(
+                'data' => array(
+                    'title' => 'Title for article',
+                    'content' => 'Content for article',
+                    'tags' => array(
+                        array('name' => 'tag10'),
+                        array('name' => 'tag11')
+                    )
+                ),
+            ),
+            array(
+                'data' => array(
+                    'title' => 'Title for article',
+                    'content' => 'Content for article',
+                    'tags' => array()
+                ),
+            )
+        );
+    }
+
 
 }
--- Tests/Form/Type/TagTypeTest.php
+++ Tests/Form/Type/TagTypeTest.php
@@ -3,8 +3,41 @@
 namespace ARV\BlogBundle\Tests\Form\Type;
 
 
+use ARV\BlogBundle\Entity\Tag;
+use ARV\BlogBundle\Form\Type\TagType;
 use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class TagTypeTest extends TypeTestCase {
+
+    public function testSubmitData()
+    {
+        $data = array(
+            'name' => 'New tag'
+        );
+
+        $type = new TagType();
+        $form = $this->factory->create($type);
+
+        $object = new Tag();
+        $accessor = PropertyAccess::createPropertyAccessor();
+        foreach ($data as $key => $value)
+        {
+            $accessor->setValue($object, $key, $value);
+        }
+
+        // submit the data to the form directly
+        $form->submit($data);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($object, $form->getData());
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($data) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
 
 }
